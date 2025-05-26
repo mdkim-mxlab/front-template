b2d6748cdea70b64fc115ff7897392bef90cb7b2
mdkim-mxlab <mdkim@mxlab.kr>
2025-05-26 17:40:17 +0900

    first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+const Home = () => {
+  return (
+    <div className="p-8 space-y-8">
+      <Button>Button</Button>
+    </div>
+  );
+};
+
+export default Home;
